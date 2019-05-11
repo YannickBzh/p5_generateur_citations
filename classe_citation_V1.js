b99e590f783ba3f778521b5b66274c3f1b3e7492
Yannick <guyader.yannick@gmail.com>
2019-05-11 08:43:33 +0200

    retrait d'espaces

--- classe_citation_V1.js
+++ classe_citation_V1.js
@@ -16,10 +16,8 @@ genererCitationTrois(){
   return this.citationc[Math.floor(Math.random() * this.citationc.length)];
 }  
 }
-
  
 const generateur = new Citations(["Au Festival des Vieilles Charrues ", "Au Hellfest à Clisson ", "Un jour à Garorock "], ["ils ont mis du temps ", "il y a toujours des gars qui tiennent ", "les programmateurs nous ont incité "], ["à trouver un distributeur d'eau.", "à montrer leur fesses.", "à aller voir Rammstein."])
-
 
 for(let i = 0; i < 3; i++){
 console.log(console.log("La citation est : " + generateur.genererCitationUne() + generateur.genererCitationDeux() + generateur.genererCitationTrois()));
