2bcd590564c758c40e7b480f5405dca65ad9a5a8
Yannick <guyader.yannick@gmail.com>
2019-08-23 11:38:02 +0200

    Suppression fichiers -> Back to 0

--- page_web/app.js
+++
@@ -1,7 +0,0 @@
-
-const $button = document.querySelector("#button")
-const $generateur = document.querySelector("#generateur")
-
-$button.addEventListener("click", () => {
-    $generateur.classList = "d-block"
-})
